d3912fefac76fbf4d82e6260062d9a158e60bbf2
caro <suoncaro@gmail.com>
2026-06-20 16:19:19 +0700

    Ex2 of lab7

--- lab7/Ex2/Ex2.cpp
+++ lab7/Ex2/Ex2.cpp
@@ -39,8 +39,76 @@ int pop(Stack* s){
     return data;
 }
 
-int 
+int peek(Stack* s){
+    if(!s || s->top == nullptr) {
+        std::cout << "Stack is empty\n";
+        return -1;
+    }
+    return s->top->data; 
+}
+
+bool isEmpty(Stack* s){
+    return s->n==0;
+}
+
+int getSize(Stack* s){
+    return s->n;
+}
+
+void clearStack(Stack* s){
+    while(s->top != nullptr){
+        pop(s);
+    }
+    return;
+}
+
+void display(Stack* s){
+    if(isEmpty(s)){
+        std::cout << "Stack is empty\n";
+        return;
+    }
+    std::cout << "Stack elements (top to bottom)\n";
+    Element* e = s->top;
+    while(e!= nullptr){
+        std::cout << e->data << " ";
+        e = e->next;
+    }
+    std::cout << "\n";
+}
 
 int main(){
-
+    Stack* s = createStack();
+    
+    // Redundant push operations
+    push(s, 10);
+    push(s, 20);
+    push(s, 30);
+    push(s, 40);
+    push(s, 50);
+    display(s);
+    
+    // Redundant push operations
+    push(s, 60);
+    push(s, 70);
+    push(s, 80);
+    display(s);
+    
+    // Redundant pop operations
+    pop(s);
+    pop(s);
+    display(s);
+    
+    // Redundant push operations
+    push(s, 90);
+    push(s, 100);
+    display(s);
+    
+    // Redundant pop operations
+    pop(s);
+    pop(s);
+    pop(s);
+    pop(s);
+    display(s);
+    
+    return 0;
 }
